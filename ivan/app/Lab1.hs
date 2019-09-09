8f37c22bb5695334966b1ebcc22c6370427d209f
Ivan Milchev <ivan.milchev@prodrive-technologies.com>
2019-09-09 20:24:53 +0200

    did exc5 correctly

--- ivan/app/Lab1.hs
+++ ivan/app/Lab1.hs
@@ -68,12 +68,19 @@ reversal = read . reverse . show
 exc4PrimeReversal :: [Integer]
 exc4PrimeReversal = [ x | x <- [1..10000], prime x, prime $ reversal x ]
 
+exc4testPrimeReversal = all (\x -> prime x && prime (reversal x)) exc4PrimeReversal
+
 -- TODO: figure out the testing part
 
 
 -- Excercise 5
+exc5primes :: Integer -> [Integer]
+exc5primes x = take 101 $ filter prime [x..]
+
+-- create a of sums of 101 consecutive primes starting from a different prime and filter only the results
+-- where the sum is also prime, then take the head
 exc5sumFirst101Primes :: Integer
-exc5sumFirst101Primes = sum $ take 101 primes
+exc5sumFirst101Primes = head ( filter prime $ map (sum . exc5primes) primes )
 
 -- TODO: testing
 
@@ -132,3 +139,8 @@ data Boy = Matthew | Peter | Jack | Arnold | Carl
             deriving (Eq,Show)
  
 boys = [Matthew, Peter, Jack, Arnold, Carl]
+
+accuses :: Boy -> Boy -> Bool
+accuses Peter Matthew = True
+accusses Peter Jack = True 
+
