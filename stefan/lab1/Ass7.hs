2791a3c5bc38e90e378d53a06e8370053479bcc9
Stefan van der Woude <stefanvdwoude@live.nl>
2019-09-09 18:48:06 +0200

    Added Ass7 (withouot test)

--- stefan/lab1/Ass7.hs
+++ stefan/lab1/Ass7.hs
@@ -1,6 +1,7 @@
 -- LAB 1
 -- Assignment 7
--- Time: 1 hour (TODO update with test)
+-- Time: 1 hour and 15 minutes (TODO update with test)
+-- Source used for specific cards: https://en.wikipedia.org/wiki/Payment_card_number#Issuer_identification_number_(IIN)
 
 module Ass7 where
 import Data.List
@@ -32,27 +33,38 @@ doubleEvenNumbers [x] = [x]
 doubleEvenNumbers (x:y:xs) = x : (y*2) : doubleEvenNumbers xs
 
 -- Check if the provided credit card number is a valid American Express number
+-- American Express card numbers are 15 numbers long and start with 34 or 37
 isAmericanExpress :: Integer -> Bool
 isAmericanExpress n = do
     let digits = convertToDigits n
     let validCard = luhn n
-    validCard && (head digits == 3) && (length digits == 15)
+    validCard && (length digits == 15) && (isAmericanExpressStart digits)
 
+isAmericanExpressStart :: [Integer] -> Bool
+isAmericanExpressStart (3:y:xs) = elem y [4, 7]
+isAmericanExpressStart xs = False
 
 -- Check if the provided credit card number is a valid MasterCard number
+-- Master card numbers are 16 numbers long and start with 51, 52, 53, 54 or 55
 isMaster :: Integer -> Bool
 isMaster n = do
     let digits = convertToDigits n
     let validCard = luhn n
-    validCard && (head digits == 5) && (length digits == 16)
+    validCard && (length digits == 16) && (isMasterStart digits)
 
+isMasterStart :: [Integer] -> Bool
+isMasterStart (5:y:xs) = elem y [1, 2, 3, 4, 5]
+isMasterStart (2:y:xs) = False  -- TODO add check
+isMasterStart xs = False
 
 -- Check if the provided credit card number is a valid Visa number
+-- Visa card numbers are 16 digits long and start with 4
 isVisa :: Integer -> Bool
 isVisa n = do
     let digits = convertToDigits n
     let validCard = luhn n
-    validCard && (head digits == 4) && (length digits == 16)
+    validCard && (length digits == 16) && (head digits == 4)
 
--- Test
--- TODO
+-- Test if the Luhn implementation returns the expected result
+testLuhnCorrectness :: Integer -> Bool -> Bool
+testLuhnCorrectness n v = v == luhn n
