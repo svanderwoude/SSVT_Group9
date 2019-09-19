22aec57d5541aacfe9e266830358ca0d37326df0
Samy Boussaa <gsamyb@gmail.com>
2019-09-19 16:17:47 +0200

    contradiction forgot $

--- samy/lab3/Main.hs
+++ samy/lab3/Main.hs
@@ -10,7 +10,7 @@ form_one :: Form
 form_one = Impl (Cnj [Neg (Dsj [Prop 1, Prop 2]), Prop 3]) (Prop 4)
 
 contradiction :: Form -> Bool
-contradiction form = not any (\ v -> evl v form) (allVals form)
+contradiction form = not $ any (\ v -> evl v form) (allVals form)
 
 tautology :: Form -> Bool
 tautology form =  all (\ v -> evl v form) (allVals form)
@@ -24,5 +24,7 @@ tautology form =  all (\ v -> evl v form) (allVals form)
 
 -- | logical equivalence -> same truth table
 
+-- Two statements X and Y are logically equivalent if X <-> Y is a tautology
+-- Source: http://sites.millersville.edu/bikenaga/math-proof/truth-tables/truth-tables.html
 equiv :: Form -> Form -> Bool
 equiv form_one form_two = tautology (Equiv form_one form_two)
