348601309b3c09547ae37c6b7d0e4b10d3d9c232
Stefan van der Woude <stefanvdwoude@live.nl>
2019-09-22 19:56:11 +0200

    Progress on ass2 and ass3

--- stefan/lab3/Ass3.hs
+++ stefan/lab3/Ass3.hs
@@ -1,6 +1,6 @@
 -- LAB 3
 -- Assignment 3
--- Time:
+-- Time: 15 minutes (so far)
 
 module Ass3 where
 import Data.List
@@ -8,4 +8,14 @@ import HelperCodeLab3
 import System.Random
 import Test.QuickCheck
 
-
+-- How to convert a boolean formula into CNF form?
+-- Step 1: remove arrows from the formula
+-- We can remove all arrows from the formula using the provided arrowfree method
+--
+-- Step 2: conversion to negation normal form
+-- We can convert the formula to negation normal form using the provided nnf
+-- method
+--
+-- Step 3: determine
+convertToCNF :: Form -> Form
+convertToCNF f = nnf (arrowfree f)
--- stefan/lab3/MainFile.hs
+++ stefan/lab3/MainFile.hs
@@ -1,9 +1,29 @@
 module MainFile where
 import Ass1
+import Ass2
 import Data.List
 import HelperCodeLab3
 import Test.QuickCheck
 
 main = do
     putStrLn "==== ASSIGNMENT 1 ===="
-    
+    putStrLn ("Test contradiction: " ++ show testContradiction)
+    putStrLn ("Test tautology: " ++ show testTautology)
+    putStrLn ("Test entails: " ++ show testEntails)
+    putStrLn ("Test equivalence: " ++ show testEquiv)
+
+    putStrLn "\n==== ASSIGNMENT 2 ===="
+    -- TODO add negatives & invalid formulas & automated tests
+    putStrLn ("Test manual prop: " ++ show (testManual "1" [Prop 1]))
+    putStrLn ("Test manual neg: " ++ show (testManual "-1" [Neg (Prop 1)]))
+    putStrLn ("Test manual cnj: " ++ show (testManual "*(1 2)" [Cnj [Prop 1, Prop 2]]))
+    putStrLn ("Test manual dsj: " ++ show (testManual "+(1 2)" [Dsj [Prop 1, Prop 2]]))
+    putStrLn ("Test manual impl: " ++ show (testManual "(1==>2)" [Impl (Prop 1) (Prop 2)]))
+    putStrLn ("Test manual equiv: " ++ show (testManual "(1<=>2)" [Equiv (Prop 1) (Prop 2)]))
+    putStrLn ("Test manual invalid: " ++ show (testManual "+-1" []))
+
+    putStrLn "\n==== ASSIGNMENT 3 ===="
+
+    putStrLn "\n==== ASSIGNMENT 4 ===="
+
+    putStrLn "\n==== ASSIGNMENT 5 ===="
