4538dbbb7d35b1f62c07cb3f7335315db96e2ce7
Ninosauleo <ninosauleo@gmail.com>
2019-10-01 22:46:16 +0200

    more details

--- submission/lab4/Ass3.hs
+++ submission/lab4/Ass3.hs
@@ -13,9 +13,17 @@ import Test.QuickCheck
 type Rel a = [(a,a)]
 
 {-
-     The closure of a relation R with respect to a property P, is the relation obtained 
-    by adding the minimum number of ordered pairs to R to obtain property P. The symmetric
-    closure is equal to the union of R with the transpose of R. In other words, the symmetric 
+    What is a closure of a relation?
+    
+    In general, let R be a relation on a set A. R may or may not have some property P, i.e. reflexivity,
+    symmetry, or transitivity. If there is a relation S with property P containing R such that S is a subset of
+    every relation with property P containing R, then S is called the closure of R with respect to P.    
+
+    What is a symmetric closure of a relation?
+
+    Rs = R ∪ R−1 = R ∪ {(b, a) | (a, b) ∈ R}.
+
+    The symmetric closure of R, Rs, is equal to the union of R with the transpose of R. In other words, the symmetric 
     closure must have (b, a) for every (a, b) in R. This means we have to create a union 
     between the given relation and a second relation where every tuple is reversed.
 -}
