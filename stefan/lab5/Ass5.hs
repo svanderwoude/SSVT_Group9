8e9162eff8b59a719a70926495c6099fb175cd7a
Stefan van der Woude <stefanvdwoude@live.nl>
2019-10-07 20:18:13 +0200

    some stuff

--- stefan/lab5/Ass5.hs
+++ stefan/lab5/Ass5.hs
@@ -1,8 +1,16 @@
 -- LAB 5
 -- Assignment 5
--- Time:
+-- Time: 15 minutes (so far)
 
 module Ass5 where
 import Data.List
+import Lecture5
 import System.Random
 import Test.QuickCheck
+
+-- Checks whether a Mersenne prime exists for prime n. Thus it checks whether
+-- 2^n - 1 is also a prime number.
+checkMersennePrime :: Integer -> Bool
+checkMersennePrime n = prime ((2^n) - 1)
+
+-- TODO everything else
--- stefan/lab5/Ass6.hs
+++ stefan/lab5/Ass6.hs
@@ -4,5 +4,13 @@
 
 module Ass6 where
 import Data.List
+import Lecture5
 import System.Random
 import Test.QuickCheck
+
+-- Tree generators taken from assignment
+tree1 n = grow (step1 n) (1,1)
+step1 n = \ (x,y) -> if x+y <= n then [(x+y,x),(x,x+y)] else [] -- step function
+
+tree2 n = grow (step2 n) (1,1)
+step2 n = \ (x,y) -> if x+y <= n then [(x+y,y),(x,x+y)] else [] -- step function
