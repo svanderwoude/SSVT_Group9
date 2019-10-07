2e84852f3271588b2e4827282cd1a3e1fbd5e05a
Stefan van der Woude <stefanvdwoude@live.nl>
2019-10-07 23:18:41 +0200

    Added submission Ass6 and Lecture5.hs

--- submission/lab5/MainFile.hs
+++ submission/lab5/MainFile.hs
@@ -1,13 +1,16 @@
 module MainFile where
-import Ass1
-import Ass2
-import Ass3
-import Ass4
-import Ass5
+-- import Ass1
+-- import Ass2
+-- import Ass3
+-- import Ass4
+-- import Ass5
 import Ass6
 import Data.List
 import Test.QuickCheck
 
 main = do
-
-    
+    putStrLn "\n==== ASSIGNMENT 6 ===="
+    putStrLn "Testing tree1:"
+    quickCheckTree1
+    putStrLn "\nTesting tree2:"
+    quickCheckTree2
